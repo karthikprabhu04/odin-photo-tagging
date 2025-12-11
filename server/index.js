0a9d2d4f3c28425ff24956e52c486281b5be2953
karthikprabhu04 <kpp32@cam.ac.uk>
2025-12-11 22:25:27 +0000

    Setup project with client and server files

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Express + PostgreSQL Server Running");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
